5cf587477b0c39a28d9cd7076125c705451f6fef
Zia-Sonata <Zia.Junaidi@sage.com>
2015-03-16 09:41:38 -0700

    init

---
+++ Sudent.BusinessRepository/FeesCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Student.Interface;
+
+namespace Sudent.BusinessRepository
+{
+   public  class FeesCalculator:IFeesCalculator
+    {
+        public int CalculateFees(int studentId)
+        {
+            return 1000;
+            //throw new NotImplementedException();
+        }
+    }
+}
