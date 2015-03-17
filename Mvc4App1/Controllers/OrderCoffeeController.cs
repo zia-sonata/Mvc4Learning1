44fe81622fc204523498c0c342e3126dd118ec6e
Zia-Sonata <Zia.Junaidi@sage.com>
2015-03-17 04:19:53 -0700

    Order Coffee Partial View

---
+++ Mvc4App1/Controllers/OrderCoffeeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc4App1.Controllers
+{
+    public class OrderCoffeeController : Controller
+    {
+        //
+        // GET: /OrderCoffee/
+
+        public ActionResult Order()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Mvc4App1/Scripts/OrderCoffee/OrderCoffee.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+
+    $("#btnOrder").click(function () {
+
+        var txt = "";
+
+        $("input:checkbox:checked").each(function () {
+
+            txt = txt + $(this).val() + " ";
+
+        });
+        $("#order").val("You ordered a coffee with: " + txt);
+
+    });
+
+    $(".btn-minimize").click(function () {
+        $(".slide").slideToggle();
+    });
+});
+
+
